fb33a7421b1db92d9ccffcb491b89583099bac50
Guka Nozadze <gukanozadze@gmail.com>
2022-11-08 18:02:08 +0400

    mutedTExt fix

--- typography/MutedTextContent.tsx
+++ typography/MutedTextContent.tsx
@@ -1,4 +1,4 @@
-export default function MutedTextContent({ text }) {
+export default function MutedTextContent({ text }: any) {
   return (
     <div className="bg-borderColor text-accentColorLight pl-3 py-1 font-bold text-sm">
       {text}
