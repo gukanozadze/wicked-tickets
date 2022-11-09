bd1c7da1cbbff1664309fcea0510af5833f340de
Guka Nozadze <gukanozadze@gmail.com>
2022-11-09 13:44:00 +0400

    Final Final Meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -3,35 +3,41 @@ export default async function Head() {
     <>
       {/* <!-- Primary Meta Tags --> */}
       <title>European Ticket Platform</title>
-      <meta name="title" content="European Ticket Platform" />
+      Meta Tags — Preview, Edit and Generate
+      <meta name="title" content="WickedTickets — Buy, Sell and Enjoy" />
       <meta
         name="description"
-        content="Buy tickets cheap with security in mind on safe platform."
+        content="
+        With WickedTickets you can 'securely' buy tickets cheap and beware of fake re-sellers from platforms like Facebook and Instagram."
       />
-
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://wicked-tickets.vercel.app/" />
-      <meta property="og:title" content="European Ticket Platform" />
+      <meta property="og:title" content="WickedTickets — Buy, Sell and Enjoy" />
       <meta
         property="og:description"
-        content="Buy tickets cheap with security in mind on safe platform."
+        content="With WickedTickets you can 'securely' buy tickets cheap and beware of fake re-sellers from platforms like Facebook and Instagram."
       />
       <meta
         property="og:image"
         content="https://images.indianexpress.com/2020/08/Bayern-Munich-vs-PSG-ucl-final.jpg"
       />
-
       {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:card"
+        content="https://images.indianexpress.com/2020/08/Bayern-Munich-vs-PSG-ucl-final.jpg"
+      />
       <meta
         property="twitter:url"
         content="https://wicked-tickets.vercel.app/"
       />
-      <meta property="twitter:title" content="European Ticket Platform" />
+      <meta
+        property="twitter:title"
+        content="WickedTickets — Buy, Sell and Enjoy"
+      />
       <meta
         property="twitter:description"
-        content="Buy tickets cheap with security in mind on safe platform."
+        content="With WickedTickets you can 'securely' buy tickets cheap and beware of fake re-sellers from platforms like Facebook and Instagram."
       />
       <meta
         property="twitter:image"
